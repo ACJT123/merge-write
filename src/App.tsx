68b863a96122852dc9772a2b0db4b1a9189a1099
ACJT123 <acjtacjt@gmail.com>
2024-08-20 17:03:27 +0800

    break

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Editor from "./pages/editor";
-import Tiptap from "./pages/editor";
 import Menubar from "./components/menubar";
 
 function App() {
   return (
-    <div>
-      <Tiptap />
-      <Menubar />
+    <div className="bg-secondary">
+      <Editor />
     </div>
   );
 }
--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,28 +1,50 @@
-// src/Tiptap.tsx
-import {
-  useEditor,
-  EditorContent,
-  FloatingMenu,
-  BubbleMenu,
-} from "@tiptap/react";
+import Highlight from "@tiptap/extension-highlight";
+import TextAlign from "@tiptap/extension-text-align";
+import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Menubar from "../components/menubar";
 
-// define your extension array
-const extensions = [StarterKit];
-
-const content = "<p>Hello World!</p>";
-
-export default function Tiptap() {
+export default function Editor() {
   const editor = useEditor({
-    extensions,
-    content,
+    extensions: [
+      StarterKit,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Highlight,
+    ],
+    content: `
+      <h3 style="text-align:center">
+        Devs Just Want to Have Fun by Cyndi Lauper
+      </h3>
+      <p style="text-align:center">
+        I come home in the morning light<br>
+        My mother says, <mark>“When you gonna live your life right?”</mark><br>
+        Oh mother dear we’re not the fortunate ones<br>
+        And devs, they wanna have fun<br>
+        Oh devs just want to have fun</p>
+      <p style="text-align:center">
+        The phone rings in the middle of the night<br>
+        My father yells, "What you gonna do with your life?"<br>
+        Oh daddy dear, you know you’re still number one<br>
+        But <s>girls</s>devs, they wanna have fun<br>
+        Oh devs just want to have
+      </p>
+      <p style="text-align:center">
+        That’s all they really want<br>
+        Some fun<br>
+        When the working day is done<br>
+        Oh devs, they wanna have fun<br>
+        Oh devs just wanna have fun<br>
+        (devs, they wanna, wanna have fun, devs wanna have)
+      </p>
+    `,
   });
 
   return (
     <>
+      <Menubar editor={editor} />
       <EditorContent editor={editor} />
-      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
     </>
   );
 }
