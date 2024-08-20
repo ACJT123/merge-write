a86b82fc2399cce4b1d01aa1c32d3540d6206e4a
ACJT123 <acjtacjt@gmail.com>
2024-08-20 16:52:16 +0800

    feat(editor): add menubar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Editor from "./pages/editor";
 import Tiptap from "./pages/editor";
+import Menubar from "./components/menubar";
 
 function App() {
   return (
     <div>
       <Tiptap />
+      <Menubar />
     </div>
   );
 }
