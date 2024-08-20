d7a424c248f8635d9202e7eb5f65d84cd2077617
ACJT123 <acjtacjt@gmail.com>
2024-08-20 15:20:26 +0800

    feat(styling): add primary and secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#152f02",
+        secondary: "#fafaf8",
+      },
+    },
   },
   plugins: [],
-}
+};
