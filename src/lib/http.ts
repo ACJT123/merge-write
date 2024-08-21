688765bd1ec71912ddb5e4b1b9cb578f61d348c4
ACJT123 <acjtacjt@gmail.com>
2024-08-21 12:41:19 +0800

    feat(lib): add http lib

---
+++ src/lib/http.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const getData = async (url: string, config?: any) => {
+  try {
+    const result = await axios.get(url, config);
+
+    return result?.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postData = async (url: string, data: any, config?: any) => {
+  try {
+    const result = await axios.post(url, data, config);
+
+    return result?.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
